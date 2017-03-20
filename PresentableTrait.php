bfe4086acff37ff5455d730c5aa1bebe94083544
asmodai <a19851234@gmail.com>
2017-03-20 10:45:38 +0800

    实现对多presenter的支持

--- PresentableTrait.php
+++ PresentableTrait.php
@@ -14,10 +14,34 @@ trait PresentableTrait
     protected $presenterInstance;
 
     protected $presenter;
+    private $_presenter_container=[];
 
     public function getShortClassName()
     {
         return substr(strrchr(__CLASS__, "\\"), 1);
+    }
+
+    public function presenterMap()
+    {
+        return [];
+    }
+
+    public function presenter($presenter_key=null, $key=null, ...$keys)
+    {
+        if (!is_null($key) && $keys) {
+            $key .= '.'.implode('.', $keys);
+        }
+
+        if (is_null($presenter_key)) {
+            return $this->present($key);
+        }
+
+        if (!isset($this->_presenter_container[$presenter_key])) {
+            $class = ArrayHelper::getValue($this->presenterMap(), $presenter_key, $presenter_key);
+            $this->_presenter_container[$presenter_key] = \Yii::createObject($class, [$this]);
+        }
+
+        return ArrayHelper::getValue($this->_presenter_container[$presenter_key], $key);
     }
 
     /**
@@ -26,20 +50,24 @@ trait PresentableTrait
      * @return mixed
      * @throws PresenterException
      */
-    public function present($key=null)
+    public function present($key=null, ...$keys)
     {
-        if (!$this->presenter) {
-            $this->presenter = __NAMESPACE__.'\\presenters\\'.$this->getShortClassName().'Presenter';
-        }
-        
-        if ( !class_exists($this->presenter))
-        {
-            throw new PresenterException('Please set the $presenter property to your presenter path.');
-        }
-
         if ( ! $this->presenterInstance)
         {
+            if (!$this->presenter) {
+                $this->presenter = __NAMESPACE__.'\\presenters\\'.$this->getShortClassName().'Presenter';
+            }
+
+            if ( !class_exists($this->presenter))
+            {
+                throw new PresenterException('Please set the $presenter property to your presenter path.');
+            }
+
             $this->presenterInstance = new $this->presenter($this);
+        }
+
+        if (!is_null($key) && $keys) {
+            $key .= '.'.implode('.', $keys);
         }
 
         return ArrayHelper::getValue($this->presenterInstance, $key);
